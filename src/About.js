e1d6fa2b35b769ad40ec39d266f8cb4830691a90
syahroelanwar <syahrul.am9773@gmail.com>
2019-11-20 10:22:39 +0700

    First Commit

---
+++ src/About.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+ 
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h2>About</h2>
+        <p>Ini Halaman About.</p>
+        
+      </div>
+    );
+  }
+}
+ 
+export default About;
---
+++ src/Contact.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+ 
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Contact</h2>
+        <p>Ini Halaman Contact.
+        </p>
+        <p>Email : syahrul.am9773@gmail.com 
+           
+</p>
+      </div>
+    );
+  }
+}
+ 
+export default Contact;
